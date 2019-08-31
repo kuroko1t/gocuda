664aa61c7dcfd77a44d9a484f070a7002cfd5040
kuroko1t <kurosawa.yk@gmail.com>
2019-08-31 21:56:44 +0900

    fix for godoc

--- cuda.go
+++ cuda.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // =============================================================================
+
 package cu
 
 // #include "/usr/local/cuda/include/cuda.h"
